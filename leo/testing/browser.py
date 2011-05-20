d3edace6ff9e665d3cca8e0c50a538caaf907e03
Taito Horiuchi <taito.horiuchi@hexagonit.fi>
2011-05-20 16:12:28 +0300

    post method added (not completed)

--- leo/testing/browser.py
+++ leo/testing/browser.py
@@ -5,7 +5,14 @@ from tempfile import gettempdir
 from Testing.ZopeTestCase.utils import startZServer
 from zope.component import getMultiAdapter
 from zope.component import getUtility
+from plone.testing._z2_testbrowser import Zope2MechanizeBrowser
+from plone.testing._z2_testbrowser import Zope2HTTPHandler
+from urllib2 import URLError
+from urllib2 import splittype
+from urllib2 import splithost
 
+
+import mechanize
 import os
 import webbrowser
 
@@ -14,6 +21,9 @@ class Browser(z2.Browser):
     """Enhanced test browser."""
 
     _base_url = None
+
+    def __init__(self, app, url=None):
+        super(z2.Browser, self).__init__(url=url, mech_browser=LeoMechanizeBrowser(app))
 
     def setBaseUrl(self, base_url):
         """Sets a base URL for all subsequent requests."""
@@ -67,3 +77,76 @@ class Browser(z2.Browser):
         with open(filepath, 'w') as file:
             file.write(self.contents)
         webbrowser.get(web_browser_name).open('file://' + filepath)
+
+#    def post(self, url, data):
+#        _z2_testbrowser.Zope2HTTPHandler = LeoHTTPHandler
+#        return super(Browser, self).post(url, data)
+
+
+class LeoMechanizeBrowser(Zope2MechanizeBrowser):
+
+#    def __init__(self, app, *args, **kws):
+#        super(LeoMechanizeBrowser, self).__init__(app, *args, **kws)
+#        def httpHandlerFactory():
+#            return LeoHTTPHandler(app)
+#        self.handler_classes["http"] = httpHandlerFactory
+
+
+    def __init__(self, app, *args, **kws):
+        def httpHandlerFactory():
+            return LeoHTTPHandler(app)
+        self.handler_classes = mechanize.Browser.handler_classes.copy()
+        self.handler_classes["http"] = httpHandlerFactory
+        self.default_others = [cls for cls in self.default_others 
+                               if cls in mechanize.Browser.handler_classes]
+        mechanize.Browser.__init__(self, *args, **kws)
+
+
+class DoRequest(Zope2HTTPHandler):
+
+    def do_request_(self, request):
+        host = request.get_host()
+        if not host:
+            raise URLError('no host given')
+
+        if request.has_data():  # POST
+            data = request.get_data()
+            if not request.has_header('Content-type'):
+                request.add_unredirected_header(
+                    'Content-type',
+                    'multipart/form-data')
+            if not request.has_header('Content-length'):
+                request.add_unredirected_header(
+                    'Content-length', '%d' % len(data))
+
+        sel_host = host
+        if request.has_proxy():
+            scheme, sel = splittype(request.get_selector())
+            sel_host, sel_path = splithost(sel)
+
+        if not request.has_header('Host'):
+            request.add_unredirected_header('Host', sel_host)
+        for name, value in self.parent.addheaders:
+            name = name.capitalize()
+            if not request.has_header(name):
+                request.add_unredirected_header(name, value)
+
+        return request
+
+class LeoHTTPHandler(DoRequest):
+
+#    def http_open(self, req):
+#        import pdb; pdb.set_trace()
+
+#    def do_request_(self, request):
+#        import pdb; pdb.set_trace()
+#        pass
+
+#    def http_request(self, request):
+#        if not request.has_header('Content-type'):
+#            request.add_unredirected_header(
+#                'Content-type',
+#                'multipart/form-data')
+#        self.do_request_(request)
+
+    http_request = DoRequest.do_request_
