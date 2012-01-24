13cb3b13a4684ea6b330973e8eee83ae04dbfd5c
Taito Horiuchi <taito.horiuchi@hexagonit.fi>
2012-01-24 14:15:55 +0200

    dependencies updated

--- hexagonit/testing/docs/HISTORY.rst
+++ hexagonit/testing/docs/HISTORY.rst
@@ -1,11 +1,17 @@
 Changelog
 =========
 
+1.1.1 (2012-01-24)
+------------------
+
+- Removed version.txt file. [taito]
+- Updated necessary dependencies to setup.py. [taito]
+- Updated frame work description on setup.py. [taito]
+
 1.1 (2011-12-09)
 ----------------
 
-Added functions to mock `datetime.datetime.now()` and `datetime.date.today()`.
-[dokai]
+- Added functions to mock `datetime.datetime.now()` and `datetime.date.today()`. [dokai]
 
 1.0.1 (Unreleased)
 ------------------
--- setup.py
+++ setup.py
@@ -4,51 +4,48 @@ from setuptools import setup
 import os
 
 
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-
-
-version = read('hexagonit', 'testing', 'version.txt').strip()
-
-long_description = (
-    read('hexagonit', 'testing', 'docs', 'README.rst'))
-
-
 long_description = (
     open(os.path.join("hexagonit", "testing", "docs", "README.rst")).read() + "\n" +
     open(os.path.join("hexagonit", "testing", "docs", "HISTORY.rst")).read() + "\n" +
     open(os.path.join("hexagonit", "testing", "docs", "CONTRIBUTORS.rst")).read()
-    )
+)
 
-setup(name='hexagonit.testing',
-      version=version,
-      description="Plone4 test helper which uses plone.testing and manuel.",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
+setup(
+    name='hexagonit.testing',
+    version='1.1.1',
+    description="Plone4 test helper which uses plone.testing and manuel.",
+    # long_description=long_description,
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
         "Programming Language :: Python",
-        ],
-      keywords='',
-      author='Hexagon IT',
-      author_email='oss@hexagonit.fi',
-      url='',
-      license='BSD',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['hexagonit'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'mock',
-          'plone.testing',
-          'setuptools',
-          'unittest2',
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
+    ],
+    keywords='',
+    author='Hexagon IT',
+    author_email='oss@hexagonit.fi',
+    url='',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['hexagonit'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'manuel',
+        'mock',
+        'plone.app.testing',
+        'plone.testing',
+        'setuptools',
+        'unittest2',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
 
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
